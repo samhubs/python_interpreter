4be2798d4f7405259faad26476e9f4ecb59d9ec4
samhubs <sameersharma.dc@gmail.com>
2024-11-03 23:03:35 -0700

    chore: initial commit

---
+++ src/lab00/lab00.py
@@ -0,0 +1,12 @@
+x = 5
+
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    return 100*20 + 28 - 4
+
+print(twenty_twenty_four())
